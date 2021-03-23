60a82f6f789435af7f769f1db535c6121da3acb1
kunal2807 <jagtiani.kunal2000@gmail.com>
2021-03-23 23:39:36 +0530

    auth setup done

--- app/models/concerns/user.rb
+++ app/models/concerns/user.rb
@@ -1,17 +1,17 @@
 class User < ActiveRecord::Base
-  has_many :articles
+  has_many :articles, dependent: :destroy
   before_save {self.email = email.downcase}
   validates :username, 
     presence: true, 
     uniqueness: {case_sensitive: false}, 
-    length: {minimum: 3, maximum: 25}
+    length: {minimum: 3, maximum: 40}
 
   VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :email, 
     presence: true,
     uniqueness: {case_sensitive: false}, 
-    length: {minimum: 3, maximum: 25},
+    length: {minimum: 3, maximum: 40},
     format: {with: VALID_EMAIL_REGEX}
 
   has_secure_password
